7870898cfea448b62d5dae1805d3846b094e5f8a
NULL <rgtntppy@gmail.com>
2024-06-22 09:53:11 +0900

    news一覧が見れるように改修

--- frontend/src/Component/News/newsInterfaces.ts
+++ frontend/src/Component/News/newsInterfaces.ts
@@ -1,4 +1,10 @@
-export interface newsListInterface {
+export interface NewsListInterface {
     menuTitle: string;
     tailFetchUrl: string;
+}
+
+export interface Story {
+    id: number;
+    title: string;
+    url: string;
 }
